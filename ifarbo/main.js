5e62e77fb3e6d9534c973e7641ba10f10d973d28
Rudy W. Quiroga G <rudyquirogag@gmail.com>
2021-03-25 16:29:52 -0400

    ADD: Agrega diferentes secciones

--- ifarbo/main.js
+++ ifarbo/main.js
@@ -52,3 +52,62 @@ right.addEventListener('click', function () {
 
 
 // Product Slider
+
+const productSlider = document.querySelector('.product-slider');
+const productLeft = document.querySelector('.product-left');
+const productRight = document.querySelector('.product-right');
+const productIndicatorParent = document.querySelector('.product-control ul');
+const productIndicators = document.querySelectorAll('.product-control li');
+productIndex = 0;
+
+productIndicators.forEach((productIndicator, i) => {
+    productIndicator.addEventListener('click', () => {
+        document.querySelector('.product-control .product-selected').classList.remove('product-selected');
+        productIndicator.classList.add('product-selected');
+        productSlider.style.transform = 'translateX(' + (i) * -(100 / 3) + '%)';
+        productIndex = i;
+    });
+});
+
+productLeft.addEventListener('click', function () {
+    productIndex = (productIndex > 0) ? productIndex - 1 : 0;
+    document.querySelector('.product-control .product-selected').classList.remove('product-selected');
+    productIndicatorParent.children[productIndex].classList.add('product-selected');
+    productSlider.style.transform = 'translateX(' + (productIndex) * -(100 / 3) + '%)';
+});
+
+productRight.addEventListener('click', function () {
+    productIndex = (productIndex < 3 - 1) ? productIndex + 1 : 2;
+    document.querySelector('.product-control .product-selected').classList.remove('product-selected');
+    productIndicatorParent.children[productIndex].classList.add('product-selected');
+    productSlider.style.transform = 'translateX(' + (productIndex) * -(100 / 3) + '%)';
+});
+
+// Section certification
+
+const bpa = document.querySelector('.bpa');
+const bpm = document.querySelector('.bpm');
+const bpf = document.querySelector('.bpf');
+const pbv = document.querySelector('.pbv');
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1
+};
+
+function callback(entries, observer) {
+    if (entries[0].isIntersecting) {
+        bpa.classList.add('certification__bpa');
+        bpm.classList.add('certification__bpm');
+        bpf.classList.add('certification__bpf');
+        pbv.classList.add('certification__pbv');
+    } else {
+        bpa.classList.remove('certification__bpa');
+        bpm.classList.remove('certification__bpm');
+        bpf.classList.remove('certification__bpf');
+        pbv.classList.remove('certification__pbv');
+    }
+}
+
+const observer = new IntersectionObserver(callback, options);
+observer.observe(bpa, bpm, bpf, pbv);
