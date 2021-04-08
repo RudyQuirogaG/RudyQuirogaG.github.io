942027a25286e0d1c34a97b27c21b5d81373a4f9
Rudy W. Quiroga G <rudyquirogag@gmail.com>
2021-04-08 09:56:19 -0400

    FIX: scripts

--- ifarbo/scripts/products.js
+++ ifarbo/scripts/products.js
@@ -1,3 +1,22 @@
+const hamburger = document.querySelector('.hamburger');
+const headerNav = document.querySelector('.header__list');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    headerNav.classList.toggle('active');
+}
+
+const headerLink = document.querySelectorAll('.header__link');
+
+headerLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove('active');
+    headerNav.classList.remove('active');
+}
+
 // Slider Products
 
 
